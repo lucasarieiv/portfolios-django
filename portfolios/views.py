3a1ea72a1cd1071db0a062c73473144d2a705e61
LucasArieiv01 <tiomedianope@gmail.com>
2017-10-09 11:23:02 -0300

    Ajustando o HTML e Css

--- portfolios/views.py
+++ portfolios/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from  .models import DadosPessoais
 # Create your views here.
 
 def portfolio_exibir(request):
-	return render(request, 'portfolios/portfolio_exibir.html')
+
+	pessoa = DadosPessoais.objects.all()
+	context = {'pessoa': pessoa} 
+	return render(request, 'portfolios/portfolio_exibir.html', context)
